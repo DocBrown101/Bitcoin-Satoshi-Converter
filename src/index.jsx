952b8afb51dc806e74684b7118aae70eba95f136
DocBrown101 <me.home@outlook.de>
2022-11-04 19:39:29 +0100

    downgrade to react 17 for compatibility reasons

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,12 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// react 18
+/* import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
@@ -9,4 +17,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); */
