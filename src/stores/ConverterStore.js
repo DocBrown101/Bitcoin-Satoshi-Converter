6785c88c5e18cfb2c09c74b5f726bd74145092f8
Daniel <me.home@outlook.de>
2023-02-04 21:45:47 +0100

    update dependencies

--- src/stores/ConverterStore.js
+++ src/stores/ConverterStore.js
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 
 const useStore = create((set) => ({
   fiatPrice: 0,
@@ -7,27 +7,27 @@ const useStore = create((set) => ({
   convertedSatoshi: undefined,
   convertedEuro: undefined,
   setFiatPrice: (fiatPrice) => {
-    set(() => ({fiatPrice: fiatPrice}));
-    set(() => ({oneFiatSats: ((1 / fiatPrice) * 100000000).toFixed(0)}));
+    set(() => ({ fiatPrice: fiatPrice }));
+    set(() => ({ oneFiatSats: ((1 / fiatPrice) * 100000000).toFixed(0) }));
   },
   onEuroInputChange: (values) => {
-    const {value} = values;
-    set((state) => ({convertedBitcoin: parseFloat((value / state.fiatPrice).toFixed(8))}));
-    set((state) => ({convertedSatoshi: ((value / state.fiatPrice) * 100000000).toFixed(0)}));
-    set(() => ({convertedEuro: undefined}));
+    const { value } = values;
+    set((state) => ({ convertedBitcoin: parseFloat((value / state.fiatPrice).toFixed(8)) }));
+    set((state) => ({ convertedSatoshi: ((value / state.fiatPrice) * 100000000).toFixed(0) }));
+    set(() => ({ convertedEuro: undefined }));
   },
   onBitcoinInputChange: (values) => {
-    const {value} = values;
-    set(() => ({convertedSatoshi: (value * 100000000).toFixed(0)}));
-    set((state) => ({convertedEuro: (value * state.fiatPrice).toFixed(2)}));
-    set(() => ({convertedBitcoin: undefined}));
+    const { value } = values;
+    set(() => ({ convertedSatoshi: (value * 100000000).toFixed(0) }));
+    set((state) => ({ convertedEuro: (value * state.fiatPrice).toFixed(2) }));
+    set(() => ({ convertedBitcoin: undefined }));
   },
   onSatoshiInputChange: (values) => {
-    const {value} = values;
-    set(() => ({convertedBitcoin: parseFloat((value / 100000000).toFixed(8))}));
-    set((state) => ({convertedEuro: ((value * state.fiatPrice) / 100000000).toFixed(2)}));
-    set(() => ({convertedSatoshi: undefined}));
-  }
+    const { value } = values;
+    set(() => ({ convertedBitcoin: parseFloat((value / 100000000).toFixed(8)) }));
+    set((state) => ({ convertedEuro: ((value * state.fiatPrice) / 100000000).toFixed(2) }));
+    set(() => ({ convertedSatoshi: undefined }));
+  },
 }));
 
 export default useStore;
--- src/stores/SimulationStore.js
+++ src/stores/SimulationStore.js
@@ -1,4 +1,4 @@
-import create from "zustand";
+import { create } from "zustand";
 
 const useStore = create((set) => ({
   sats: ((10 / 55000) * 100000000).toFixed(0),
@@ -9,30 +9,18 @@ const useStore = create((set) => ({
   onFiatPriceChange: (values) => {
     const { value } = values;
     set(() => ({ fiatPrice: parseFloat(value) }));
-    set((state) => ({
-      sats: ((state.fiatPrice / state.sliderPrice) * 100000000).toFixed(0),
-    }));
-    set((state) => ({
-      oneFiatSats: ((1 / state.sliderPrice) * 100000000).toFixed(0),
-    }));
+    set((state) => ({ sats: ((state.fiatPrice / state.sliderPrice) * 100000000).toFixed(0) }));
+    set((state) => ({ oneFiatSats: ((1 / state.sliderPrice) * 100000000).toFixed(0) }));
   },
   onSliderPriceChange: (event, newValue) => {
     set(() => ({ sliderPrice: newValue }));
-    set((state) => ({
-      sats: ((state.fiatPrice / state.sliderPrice) * 100000000).toFixed(0),
-    }));
-    set((state) => ({
-      oneFiatSats: ((1 / state.sliderPrice) * 100000000).toFixed(0),
-    }));
+    set((state) => ({ sats: ((state.fiatPrice / state.sliderPrice) * 100000000).toFixed(0) }));
+    set((state) => ({ oneFiatSats: ((1 / state.sliderPrice) * 100000000).toFixed(0) }));
   },
   onInputChange: (event) => {
     set(() => ({ sliderPrice: event.target.value }));
-    set((state) => ({
-      sats: ((state.fiatPrice / state.sliderPrice) * 100000000).toFixed(0),
-    }));
-    set((state) => ({
-      oneFiatSats: ((1 / state.sliderPrice) * 100000000).toFixed(0),
-    }));
+    set((state) => ({ sats: ((state.fiatPrice / state.sliderPrice) * 100000000).toFixed(0) }));
+    set((state) => ({ oneFiatSats: ((1 / state.sliderPrice) * 100000000).toFixed(0) }));
   },
 }));
 
