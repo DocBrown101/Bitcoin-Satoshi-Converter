4da7a8ddd707996736bb24a24cd9b57b65032c3f
DocBrown101 <docbrown@tutanota.de>
2025-09-28 21:58:00 +0200

    update

--- src/components/MempoolWs.jsx
+++ src/components/MempoolWs.jsx
@@ -2,21 +2,12 @@ import React, {useEffect, useState} from "react";
 import {NumericFormat} from 'react-number-format';
 import {useTranslation} from "react-i18next";
 
-import {Grid, Accordion, AccordionSummary, AccordionDetails, Box, Typography, Container, Paper} from '@mui/material';
+import {Grid, Accordion, Skeleton, AccordionSummary, AccordionDetails, Box, Typography, Container, Paper} from '@mui/material';
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {styled} from '@mui/material/styles';
 
 import ConverterStore from "../stores/ConverterStore";
 import calculateHalvingData from '../services/HalvingCalculator';
-
-const Item = styled(Paper)(({theme}) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1e1e1e' : '#fff',
-  ...theme.typography.body1,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
 
 const formatDate = (date, timeZone, locales) => {
   return (
@@ -99,110 +90,109 @@ export default function MempoolWsComponent({...props}) {
   }, []);
 
   const {t} = useTranslation();
+  const isLoading = !webSocketReady || !lastFeeData || !lastBlockData;
+  const loadingTitle = !webSocketReady ? t("PleaseWait") : "Waiting for message from server ...";
+  const normalTitle = "Bitcoin Halving Event";
 
-  if (!webSocketReady) {
-    return <PlaceholderComponent text={t("PleaseWait")} />;
-  } else if (!lastFeeData || !lastBlockData) {
-    return <PlaceholderComponent text="Waiting for message from server ..." />;
-  } else {
-    return (
-      <React.Fragment>
-        <HalvingComponent blockData={lastBlockData} timeAvg={lastTimeAvg} />
-        <CurrentFeesComponent feeData={lastFeeData} />
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <Box mb={2}>
+        <Container maxWidth="sm">
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography variant="h6">{isLoading ? loadingTitle : normalTitle}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography mb={2}>{t("HalvingText.line1")}</Typography>
+              <Typography mb={2}>{t("HalvingText.line2")}</Typography>
+              <Typography>{t("HalvingText.line3")}</Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Paper>
+            <HalvingComponent blockData={lastBlockData} timeAvg={lastTimeAvg} isLoading={isLoading} />
+          </Paper>
+        </Container>
+      </Box>
+      <Box mb={2}>
+        <Container maxWidth="sm">
+          <Typography variant="h6">{t("CurrentTransactionFees")}</Typography>
+          <CurrentFeesComponent feeData={lastFeeData} isLoading={isLoading} />
+        </Container>
+      </Box>
+    </React.Fragment>
+  );
 }
 
-const PlaceholderComponent = React.memo((props) => {
-  return (
-    <Box mb={2}>
-      <Container maxWidth="sm">
-        <Typography variant="h6">{props.text}</Typography>
-        <Paper>
-          <Box sx={{height: 352}} />
-        </Paper>
-      </Container>
-    </Box>
-  );
-});
+const HalvingComponent = ({blockData, timeAvg, isLoading}) => {
+  if (isLoading) {
+    return (<Skeleton variant="rounded" height={120} />);
+  }
 
-const HalvingComponent = React.memo((props) => {
   const {t} = useTranslation();
-
-  const halvingData = calculateHalvingData(props.timeAvg, props.blockData.height);
+  const halvingData = calculateHalvingData(timeAvg, blockData.height);
   const date = formatDate(halvingData.estimatedDate, t("TimeZone"), t("Locales"));
 
   return (
-    <Box mb={2}>
-      <Container maxWidth="sm">
-        <Accordion>
-          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography variant="h6">Bitcoin Halving Event</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography paragraph={true}>{t("HalvingText.line1")}</Typography>
-            <Typography paragraph={true}>{t("HalvingText.line2")}</Typography>
-            <Typography>{t("HalvingText.line3")}</Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Paper>
-          <Box fontFamily="Monospace" fontWeight="fontWeightBold" textAlign="center" p={2}>
-            <Typography color="secondary" variant="h6">
-              <NumericFormat value={halvingData.blocksToNextHalving}
-                displayType={'text'}
-                decimalScale={0}
-                fixedDecimalScale={true}
-                suffix={t("MissingBlocks")}
-                thousandSeparator={'.'}
-                decimalSeparator={','} />
-            </Typography>
-            <Typography>{halvingData.estimatedDays} days {halvingData.estimatedHoures} hours {halvingData.estimatedMinutes} minutes</Typography>
-            <Typography variant="h6" color="secondary">{date}</Typography>
-          </Box >
-        </Paper>
-      </Container>
-    </Box>
+    <Box fontFamily="Monospace" fontWeight="fontWeightBold" textAlign="center" p={2}>
+      <Typography color="secondary" variant="h6">
+        <NumericFormat value={halvingData.blocksToNextHalving}
+          displayType={'text'}
+          decimalScale={0}
+          fixedDecimalScale={true}
+          suffix={t("MissingBlocks")}
+          thousandSeparator={'.'}
+          decimalSeparator={','} />
+      </Typography>
+      <Typography>{halvingData.estimatedDays} days {halvingData.estimatedHoures} hours {halvingData.estimatedMinutes} minutes</Typography>
+      <Typography variant="h6" color="secondary">{date}</Typography>
+    </Box >
   );
-});
+};
 
-const CurrentFeesComponent = React.memo((props) => {
+const CurrentFeesComponent = ({feeData, isLoading}) => {
+  if (isLoading) {
+    return (<Skeleton variant="rounded" height={184} />);
+  }
+
   const eurFiatPrice = ConverterStore((state) => state.eurFiatPrice);
-  const fastest = ((props.feeData.fastestFee * 140 * eurFiatPrice) / 100000000).toFixed(2);
-  const medium = ((props.feeData.hourFee * 140 * eurFiatPrice) / 100000000).toFixed(2);
-  const minimum = ((props.feeData.minimumFee * 140 * eurFiatPrice) / 100000000).toFixed(2);
+  const fastest = ((feeData.fastestFee * 140 * eurFiatPrice) / 100000000).toFixed(2);
+  const medium = ((feeData.hourFee * 140 * eurFiatPrice) / 100000000).toFixed(2);
+  const minimum = ((feeData.minimumFee * 140 * eurFiatPrice) / 100000000).toFixed(2);
   const {t} = useTranslation();
   return (
-    <Box mb={2}>
-      <Container maxWidth="sm">
-        <Typography variant="h6">{t("CurrentTransactionFees")}</Typography>
-        <Paper>
-          <Box fontFamily="Monospace" fontWeight="fontWeightBold" p={2}>
-            <Grid container spacing={2}>
-              <Grid size={6}>
-                <Item>{t("HighPriority")}</Item>
-              </Grid>
-              <Grid size={6}>
-                <Item>{props.feeData.fastestFee} sat/vB = {fastest.replace(".", ",")} €</Item>
-              </Grid>
+    <Paper>
+      <Box fontFamily="Monospace" fontWeight="fontWeightBold" p={2}>
+        <Grid container spacing={2}>
+          <TextPaperCell>{t("HighPriority")}</TextPaperCell>
+          <TextPaperCell>{feeData.fastestFee} sat/vB = {fastest.replace(".", ",")} €</TextPaperCell>
 
-              <Grid size={6}>
-                <Item>{t("MediumPriority")}</Item>
-              </Grid>
-              <Grid size={6}>
-                <Item>{props.feeData.hourFee} sat/vB = {medium.replace(".", ",")} €</Item>
-              </Grid>
+          <TextPaperCell>{t("MediumPriority")}</TextPaperCell>
+          <TextPaperCell>{feeData.hourFee} sat/vB = {medium.replace(".", ",")} €</TextPaperCell>
 
-              <Grid size={6}>
-                <Item>{t("NoPriority")}</Item>
-              </Grid>
-              <Grid size={6}>
-                <Item>{props.feeData.minimumFee} sat/vB = {minimum.replace(".", ",")} €</Item>
-              </Grid>
-            </Grid>
-          </Box >
-        </Paper>
-      </Container>
-    </Box>
+          <TextPaperCell>{t("NoPriority")}</TextPaperCell>
+          <TextPaperCell>{feeData.minimumFee} sat/vB = {minimum.replace(".", ",")} €</TextPaperCell>
+        </Grid>
+      </Box >
+    </Paper>
   );
-});
+};
+
+const TextPaperCell = ({children}) => {
+  return (
+    <Grid size={6}>
+      <Paper
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'dark' ? '#1e1e1e' : '#fff',
+          padding: 1,
+          fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+          fontSize: '1rem',
+          fontWeight: 400,
+          textAlign: 'center',
+          color: 'text.secondary',
+        }}>
+        {children}
+      </Paper>
+    </Grid>
+  );
+};
